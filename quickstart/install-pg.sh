9d55435a388fcd6c18192fc4e692922cbda9fe24
Sami I <48630255+samimseih@users.noreply.github.com>
2023-08-09 18:56:38 -0500

    Update install-pg.sh

--- quickstart/install-pg.sh
+++ quickstart/install-pg.sh
@@ -19,7 +19,6 @@ function install_prereqs()
 		libxslt \
 		autoconf-archive \
 		docbook-xsl \
-  		libicu-devel \
 		libtool -y
 
 	sudo dnf install perl -y
@@ -34,14 +33,15 @@ function install_pg()
 	if [[ "$version" == "current" ]];
 	then
         	git clone https://github.com/postgres/postgres.git
+		cd postgres
 	else
         	git clone \
 			-c advice.detachedHead=false  \
-			--branch REL_${version} https://git.postgresql.org/git/postgresql.git $directory/postgres
+			--branch REL_${version} https://git.postgresql.org/git/postgresql.git $directory/postgresql
+		cd postgresql
 	fi;
 
-	cd postgres
-	./configure --prefix $directory/pghome
+	./configure --prefix $directory/pghome --with-openssl
 	make install -j $(cat /proc/cpuinfo  | grep processor | tail -1 | awk '{print $2}' FS=":")
 	cd $MYPWD
 }
@@ -53,7 +53,18 @@ function create_prof()
 	echo export 'LD_LIBRARY_PATH=$PGHOME/lib:$LD_LIBRARY_PATH' >> $directory/activate.sh
 	echo export PGDATA=$directory/pgdata >> $directory/activate.sh
 	echo export PGUSER=postgres >> $directory/activate.sh
+	echo export PGPORT=5432 >> $directory/activate.sh
 	chmod a+x $directory/activate.sh
+}
+function create_prof_replica()
+{
+	echo export PGHOME=$directory/pghome > $directory/activate_sec.sh
+	echo export 'PATH=$PGHOME/bin:$PATH' >> $directory/activate_sec.sh
+	echo export 'LD_LIBRARY_PATH=$PGHOME/lib:$LD_LIBRARY_PATH' >> $directory/activate_sec.sh
+	echo export PGDATA=$directory/pgdata_sec >> $directory/activate_sec.sh
+	echo export PGUSER=postgres >> $directory/activate_sec.sh
+	echo export PGPORT=5433 >> $directory/activate_sec.sh
+	chmod a+x $directory/activate_sec.sh
 }
 
 function create_db()
@@ -70,10 +81,36 @@ function create_db()
 	echo archive_command = \'test \! -f $directory/archive_logs/%f \&\& \
 		cp %p $directory/archive_logs/%f\'  >> $PGDATA/postgresql.conf
 	echo logging_collector = on >> $PGDATA/postgresql.conf
+	echo max_wal_senders = 5 >> $PGDATA/postgresql.conf
+	echo host replication   all   all  trust >> $PGDATA/pg_hba.conf
 	pg_ctl start
 }
 
-install_prereqs
-install_pg
-create_prof
-create_db
+function create_replica()
+{
+	source $directory/activate.sh
+	rm -rfv ${PGDATA}_sec >/dev/null
+	pg_basebackup \
+		--pgdata=${PGDATA}_sec \
+		--format=p \
+		--write-recovery-conf \
+		--checkpoint=fast \
+		--label=mffb \
+		--progress \
+		--username=$PGUSER \
+		--create-slot \
+		--slot=pgenv_slot
+	export PGDATA=${PGDATA}_sec
+	echo port=5433 >> $PGDATA/postgresql.conf
+	echo archive_mode = off >> $PGDATA/postgresql.conf
+	echo restore_command = \'\' >> $PGDATA/postgresql.conf
+	echo archive_command = \'\' >> $PGDATA/postgresql.conf
+	pg_ctl start
+}
+
+#install_prereqs
+#install_pg
+#create_prof
+#create_db
+#create_replica
+#create_prof_replica
